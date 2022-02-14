b61300352e71f7c622c17b3c1f4294be013ddd8c
miguelmartinn9 <nerviosus@protonmail.com>
2022-02-14 11:12:59 -0600

    Changed string id and set correct layout for pubkey scan

--- app/src/main/java/com/samourai/sentinel/ui/fragments/AddNewPubKeyBottomSheet.kt
+++ app/src/main/java/com/samourai/sentinel/ui/fragments/AddNewPubKeyBottomSheet.kt
@@ -53,7 +53,7 @@ class AddNewPubKeyBottomSheet(private val pubKey:String= "") : GenericBottomShee
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.bottom_sheet, container)
+        val view = inflater.inflate(R.layout.layout_bottom_sheet, container)
         view.findViewById<TextView>(R.id.dialogTitle).text = "Add new public key"
         val fragmentLayout = inflater.inflate(R.layout.fragment_bottomsheet_view_pager, container)
         val content = view.findViewById<FrameLayout>(R.id.contentContainer)
