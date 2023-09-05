12aab1c422d17064cda5991ec68a4d6482775512
miguel <miguelm9@protonmail.com>
2023-09-05 11:23:31 +0200

    bump density by reducng the maxfragment length

--- app/src/main/java/com/samourai/sentinel/ui/collectionDetails/send/SendFragment.kt
+++ app/src/main/java/com/samourai/sentinel/ui/collectionDetails/send/SendFragment.kt
@@ -594,7 +594,7 @@ class SendFragment : Fragment() {
     private fun generateQRCode(uri: String) {
         viewModel.viewModelScope.launch(Dispatchers.Default) {
             withContext(Dispatchers.Main) {
-                fragmentSpendBinding.fragmentBroadcastTx.psbtQRCode.maxFragmentLength = 18
+                fragmentSpendBinding.fragmentBroadcastTx.psbtQRCode.maxFragmentLength = 10
                 fragmentSpendBinding.fragmentBroadcastTx.psbtQRCode.fps = 4
                 fragmentSpendBinding.fragmentBroadcastTx.psbtQRCode.setContent(UR.fromBytes(RegistryType.CRYPTO_PSBT.type,Hex.decode(uri)));
             }
