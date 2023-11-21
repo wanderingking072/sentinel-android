66d0678757e5cd83bcc9676118dc3adce4214b26
miguel <miguel@samourai.is>
2023-11-21 16:11:51 +0100

    remove home screen menu items

--- app/src/main/java/com/samourai/sentinel/ui/home/HomeActivity.kt
+++ app/src/main/java/com/samourai/sentinel/ui/home/HomeActivity.kt
@@ -424,18 +424,6 @@ class HomeActivity : SentinelActivity() {
         }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.home_options_settings -> {
-                startActivity(Intent(this, SettingsActivity::class.java))
-            }
-            R.id.home_options_broadcast -> {
-                startActivity(Intent(this, BroadcastTx::class.java))
-            }
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.home_menu, menu)
         return super.onCreateOptionsMenu(menu)
